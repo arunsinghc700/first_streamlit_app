ece475fe0401e0bbc4d65ffe4e60b78de6f2d12b
arunsinghc700 <111640102+arunsinghc700@users.noreply.github.com>
2022-08-22 17:55:06 +0530

    Update streamlit_app.py
    read json data and display to grid

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,4 +28,11 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# write your own comment -what does the next line do? 
+# convert parse jjson data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+# display the data in grid page
+streamlit.dataframe(fruityvice_normalized)
+
 
